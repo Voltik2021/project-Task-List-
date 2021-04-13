80f948ba1cdf22bcd826ce2ef694aaf5b4dc2b2b
Voltik <Voltik2019@yandex.ru>
2021-04-13 17:02:11 +0300

    100%

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ let buttonAdd = document.querySelector('.button');
 let taskList = document.querySelector('.task-list');
 let list = document.querySelector('.task-list').firstElementChild;
 let toClose = document.querySelector('.toClose')
-let grayDOWNarrow = document.querySelector('.grayDOWNarrow');
-let blackUParrow = document.querySelector('.blackUParrow');
-let blackDOWNarrow = document.querySelector('.blackDOWNarrow');
-let grayUParrow = document.querySelector('.grayUParrow');
+let grayDOWNarrow = document.querySelector('.form-img').firstElementChild;
+console.log(grayDOWNarrow)
+
 
 
 
@@ -14,11 +13,11 @@ let grayUParrow = document.querySelector('.grayUParrow');
 
 buttonAdd.addEventListener('click', () => {
     let clonList = list.cloneNode(true);
-    clonList.firstElementChild.value = '';    
+    clonList.firstElementChild.value = '';
     taskList.append(clonList);
     clonList.lastElementChild.addEventListener('click', (e) => {
         clonList.remove();
-    });    
+    });
 });
 
 toClose.addEventListener('click', (e) => {
@@ -26,42 +25,25 @@ toClose.addEventListener('click', (e) => {
 });
 
 grayDOWNarrow.addEventListener('click', (e) => {
-    let htmlColection = taskList.querySelectorAll('li');
-    changingTheSorting('blackUParrow') 
-    sortArr(htmlColection, 'true'); 
+    changingTheSorting(e.target.classList.contains('grayDOWNarrow'));
+
 });
 
-blackUParrow.addEventListener('click', (e) => {
+function changingTheSorting(type) {
     let htmlColection = taskList.querySelectorAll('li');
-    changingTheSorting('blackDOWNarrow') 
-    sortArr(htmlColection); 
-});
-
-blackDOWNarrow.addEventListener('click', (e) => {
-    let htmlColection = taskList.querySelectorAll('li');
-    changingTheSorting('upAgain') 
-    sortArr(htmlColection, 'true'); 
-});
-
-
-
-function changingTheSorting(type) {    
-    blackDOWNarrow.style.display = 'none';
-    grayUParrow.style.display = 'none';
-    grayDOWNarrow.style.display = 'none';
-    blackUParrow.style.display = 'none';
-
     switch (type) {
-        case 'blackUParrow': 
-            blackUParrow.style.display = 'block';
+        case true:
+            sortArr(htmlColection, 'true');
+            grayDOWNarrow.classList.add('grayUParrow');
+            grayDOWNarrow.classList.remove('grayDOWNarrow');
             break;
-        case 'blackDOWNarrow':
-            blackDOWNarrow.style.display = 'block';
-            break;  
-        case 'upAgain':
-            blackUParrow.style.display = 'block';
-            break;  
-    };   
+        case false:
+            sortArr(htmlColection);
+            let grayUParrow = document.querySelector('.grayUParrow');
+            grayUParrow.classList.add('grayDOWNarrow');
+            grayUParrow.classList.remove('grayUParrow');
+            break;
+    };
 };
 
 
@@ -70,17 +52,17 @@ function sortArr(htmlColection, direction) {
     let newArr = arr.map((item) => {
         return item.firstElementChild.value;
     });
-    
-    newArr.sort((a, b) => {                
+
+    newArr.sort((a, b) => {
         if (a < b) {
-            return direction === 'true'? -1:1;
+            return direction === 'true' ? -1 : 1;
         } else if (a > b) {
-            return direction === 'true'? 1:-1;
+            return direction === 'true' ? 1 : -1;
         } else {
             return 0;
         }
-    }); 
+    });
     for (i = 0; i < arr.length; i++) {
         arr[i].firstElementChild.value = newArr[i]
-    }       
+    }
 };
